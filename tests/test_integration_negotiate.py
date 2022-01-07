b545df409da3322d802d0fea3ddf1309ab12fdbe
Andy Grigg <andrew.grigg@ansys.com>
2022-01-07 16:03:08 -0500

    Try adding krb ports

--- tests/test_integration_negotiate.py
+++ tests/test_integration_negotiate.py
@@ -22,7 +22,7 @@ from .integration.common import (
 TEST_URL = f"http://test-server:{TEST_PORT}"
 
 app = FastAPI()
-authenticated_app = SPNEGOAuthMiddleware(app, hostname="")
+authenticated_app = SPNEGOAuthMiddleware(app, hostname="kdc-server")
 
 
 @app.patch("/models/{model_id}")
