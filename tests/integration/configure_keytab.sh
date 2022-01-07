33a859b29c82874ad0402bb4a8a012f97f10f40e
Doug Addy <doug.addy@ansys.com>
2022-01-07 14:47:04 +0000

    Remove sudo calls from configure_keytab.sh

--- tests/integration/configure_keytab.sh
+++ tests/integration/configure_keytab.sh
@@ -1,10 +1,9 @@
-sudo sed -i '1s/^/127.0.0.1    test-server\n/' /etc/hosts
+sed -i '1s/^/127.0.0.1    test-server\n/' /etc/hosts
 ktutil <<'EOF_'
 read_kt /tmp/keytabs/user.keytab
 read_kt /tmp/keytabs/service.keytab
 write_kt /tmp/keytabs/krb5.keytab
 EOF_
-sudo cp /tmp/keytabs/krb5.keytab /etc/krb5.keytab
-sudo chmod 644 /etc/krb5.keytab
+cp /tmp/keytabs/krb5.keytab /etc/krb5.keytab
+chmod 644 /etc/krb5.keytab
 kinit httpuser@EXAMPLE.COM -k -t /tmp/keytabs/user.keytab
-
