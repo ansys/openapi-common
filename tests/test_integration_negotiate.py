88c3ec3abcb020acd3820da40416b1a543074e57
Andy Grigg <andrew.grigg@ansys.com>
2022-01-07 15:41:56 -0500

    Try removing hostname from middleware init

--- tests/test_integration_negotiate.py
+++ tests/test_integration_negotiate.py
@@ -22,7 +22,7 @@ from .integration.common import (
 TEST_URL = f"http://test-server:{TEST_PORT}"
 
 app = FastAPI()
-authenticated_app = SPNEGOAuthMiddleware(app, hostname="test-server")
+authenticated_app = SPNEGOAuthMiddleware(app)
 
 
 @app.patch("/models/{model_id}")
