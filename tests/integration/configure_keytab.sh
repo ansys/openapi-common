683d5f97f5084396c09e5342afd2884c727d7621
Doug Addy <doug.addy@ansys.com>
2022-01-07 16:25:24 +0000

    Chown files before reading them

--- tests/integration/configure_keytab.sh
+++ tests/integration/configure_keytab.sh
@@ -1,6 +1,4 @@
 sed -i '1s/^/127.0.0.1    test-server\n/' /etc/hosts
-chmod 644 /tmp/keytabs/user.keytab
-chmod 644 /tmp/keytabs/service.keytab
 ktutil <<'EOF_'
 read_kt /tmp/keytabs/user.keytab
 read_kt /tmp/keytabs/service.keytab
