32b8e57fdebd529074f07bedea374d33c0e70eb9
Doug Addy <da1910@users.noreply.github.com>
2022-01-10 13:25:15 +0000

    19 - Kerberos Integration Tests (#47)
    * Add kerberos tests
    
    * Run black
    
    * Fixup github actions with two containers
    
    * Add required environment variables
    
    * Use container image from kdc-container repo
    
    * Specify container manually, see https://github.community/t/env-variables-in-uses/17466/2
    
    * Remove registry from service container definition
    
    * Make path relative for configure_keytab.sh
    
    * Move keytabs before running ktutil
    
    * Set execute permission on configure_keytab
    
    * Configure kerberos packages
    
    * Run apt update
    
    * Install kerberos then set config
    
    * Remove extra info that MIT is happy with but not heimdal
    
    * Add port mappings
    
    * Update to use service host name
    
    * Set middleware ctor hostname to kdc service hostname
    
    * Reinstate realm config
    
    * Set alternative realm and kinit the service principal
    
    * Copy the right keytab
    
    * Use terminate instead of kill to support 3.6
    
    * Pin old release of tox-gh-actions
    
    * Rename test class and optimize imports
    
    * Skip kerberos tests on windows, with dependencies. Add container setup for coverage runner.
    
    * Fix reference to build vs. main in ci_cd.yml
    
    * Run black
    
    * Switch container for coverage measurement
    
    Co-authored-by: Andy Grigg <andrew.grigg@ansys.com>

---
+++ tests/integration/configure_keytab.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo "------------------------------------------------"
+echo "---- Configuring /etc/hosts --------------------"
+echo "------------------------------------------------"
+sed '1s/^/127.0.0.1    test-server\n/' /etc/hosts > ./hosts
+cp ./hosts /etc/hosts
+
+echo "------------------------------------------------"
+echo "---- Copying keytabs to writeable location -----"
+echo "------------------------------------------------"
+cp /tmp/keytabs/user.keytab ./user.keytab
+cp /tmp/keytabs/service.keytab ./service.keytab
+
+echo "------------------------------------------------"
+echo "---- Setting file permissions and owner --------"
+echo "------------------------------------------------"
+chown -R root:root ./user.keytab
+chown -R root:root ./service.keytab
+chmod 644 ./user.keytab
+chmod 644 ./service.keytab
+
+echo "------------------------------------------------"
+echo "---- Writing combined keytab file --------------"
+echo "------------------------------------------------"
+ktutil <<'EOF_'
+read_kt ./user.keytab
+read_kt ./service.keytab
+write_kt ./krb5.keytab
+EOF_
+
+echo "------------------------------------------------"
+echo "---- Moving keytab and setting permissions -----"
+echo "------------------------------------------------"
+cp ./krb5.keytab /etc/krb5.keytab
+chmod 644 /etc/krb5.keytab
+
+echo "------------------------------------------------"
+echo "---- Initialize ticket cache -------------------"
+echo "------------------------------------------------"
+kinit httpuser@EXAMPLE.COM -k -t ./user.keytab
+kinit HTTP/test-server@EXAMPLE.COM -k -t ./service.keytab
