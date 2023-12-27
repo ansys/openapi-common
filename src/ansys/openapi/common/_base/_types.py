0b4b7bdc6fb304d31be2126cb4aae0418dd2f088
Ludovic Steinbach <ludovic.steinbach@ansys.com>
2023-12-27 16:08:13 +0100

    Code formatting

--- src/ansys/openapi/common/_base/_types.py
+++ src/ansys/openapi/common/_base/_types.py
@@ -45,18 +45,20 @@ class ModelBase(metaclass=abc.ABCMeta):
         for attr in self.swagger_types.keys():
             value = getattr(self, attr)
             if isinstance(value, list):
-                result[attr] = list(map(
-                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
-                    value
-                ))
+                result[attr] = list(
+                    map(lambda x: x.to_dict() if hasattr(x, "to_dict") else x, value)
+                )
             elif hasattr(value, "to_dict"):
                 result[attr] = value.to_dict()
             elif isinstance(value, dict):
-                result[attr] = dict(map(
-                    lambda item: (item[0], item[1].to_dict())
-                    if hasattr(item[1], "to_dict") else item,
-                    value.items()
-                ))
+                result[attr] = dict(
+                    map(
+                        lambda item: (item[0], item[1].to_dict())
+                        if hasattr(item[1], "to_dict")
+                        else item,
+                        value.items(),
+                    )
+                )
             elif isinstance(value, Enum):
                 result[attr] = value.value
             else:
--- tests/test_model_methods.py
+++ tests/test_model_methods.py
@@ -4,7 +4,6 @@ import pytest
 
 
 class TestToDict:
-
     @pytest.mark.parametrize(
         ["model_instance", "expected_dict"],
         [
@@ -15,10 +14,10 @@ class TestToDict:
                     "int_property": 3,
                     "bool_property": False,
                     "list_property": ["It's", "a", "list"],
-                }
+                },
             ),
             (models.ExampleBaseModel("foo"), {"model_type": "foo"}),
-        ]
+        ],
     )
     def test_model_to_dict(self, model_instance, expected_dict):
         assert model_instance.to_dict() == expected_dict
@@ -34,9 +33,12 @@ class TestToStr:
     @pytest.mark.parametrize(
         ["model_instance", "expected_result"],
         [
-            (models.ExampleModel("foo", 3, False, ["It's", "a", "list"]), expected_str_model),
+            (
+                models.ExampleModel("foo", 3, False, ["It's", "a", "list"]),
+                expected_str_model,
+            ),
             (models.ExampleBaseModel("foo"), "{'model_type': 'foo'}"),
-        ]
+        ],
     )
     def test_model_to_str(self, model_instance, expected_result):
         assert model_instance.to_str() == expected_result
