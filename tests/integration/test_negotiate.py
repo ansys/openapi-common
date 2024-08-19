c265c8cb154a128d960c9f7f47633e36a35075f1
Andy Grigg <andrew.grigg@ansys.com>
2024-08-19 16:26:10 -0400

    Reorg negotiate tests further

--- tests/integration/test_negotiate.py
+++ tests/integration/test_negotiate.py
@@ -125,6 +125,16 @@ class NegotiateTests:
         assert response == deserialized_response
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="No portable KDC is available at present")
+class NegotiateFailureTests:
+    def test_bad_principal_returns_403(self, auth_mode):
+        client_factory = ApiClientFactory(TEST_URL, SessionConfiguration())
+        with pytest.raises(ApiConnectionException) as excinfo:
+            _ = client_factory.with_autologon(authentication_scheme=auth_mode).connect()
+        assert excinfo.value.response.status_code == 403
+        assert excinfo.value.response.reason == "Forbidden"
+
+
 @pytest.mark.parametrize("auth_mode", [AuthenticationScheme.AUTO, AuthenticationScheme.KERBEROS])
 class TestNegotiate(NegotiateTests):
     @pytest.fixture(autouse=True)
@@ -137,8 +147,8 @@ class TestNegotiate(NegotiateTests):
             sleep(1)
 
 
-@pytest.mark.skipif(sys.platform == "win32", reason="No portable KDC is available at present")
-class TestNegotiateFailures:
+@pytest.mark.parametrize("auth_mode", [AuthenticationScheme.AUTO, AuthenticationScheme.KERBEROS])
+class TestNegotiateFailures(NegotiateTests):
     @pytest.fixture(autouse=True)
     def server(self):
         # Remove all the routes (a bit drastic)
@@ -156,16 +166,6 @@ class TestNegotiateFailures:
         while proc.is_alive():
             sleep(1)
 
-    @pytest.mark.parametrize(
-        "auth_mode", [AuthenticationScheme.AUTO, AuthenticationScheme.KERBEROS]
-    )
-    def test_bad_principal_returns_403(self, auth_mode):
-        client_factory = ApiClientFactory(TEST_URL, SessionConfiguration())
-        with pytest.raises(ApiConnectionException) as excinfo:
-            _ = client_factory.with_autologon(authentication_scheme=auth_mode).connect()
-        assert excinfo.value.response.status_code == 403
-        assert excinfo.value.response.reason == "Forbidden"
-
 
 @pytest.mark.parametrize("auth_mode", [AuthenticationScheme.KERBEROS])
 class TestNegotiateWrongHeader(NegotiateTests):
@@ -182,9 +182,53 @@ class TestNegotiateWrongHeader(NegotiateTests):
 
 
 @pytest.mark.parametrize("auth_mode", [AuthenticationScheme.KERBEROS])
+class TestNegotiateWrongHeaderFailures(NegotiateTests):
+    @pytest.fixture(autouse=True)
+    def server(self):
+        # Remove all the routes (a bit drastic)
+        custom_test_app.router.routes = []
+
+        @custom_test_app.get("/")
+        async def get_forbidden(request: Request):
+            validate_user_principal(request, "otheruser@EXAMPLE.COM")
+            return None
+
+        os.environ["change-header"] = "1"
+        proc = Process(target=run_server, args=(), daemon=True)
+        proc.start()
+        yield
+        proc.terminate()
+        del os.environ["change-header"]
+        while proc.is_alive():
+            sleep(1)
+
+
+@pytest.mark.parametrize("auth_mode", [AuthenticationScheme.KERBEROS])
 class TestNegotiateMissingHeader(NegotiateTests):
     @pytest.fixture(autouse=True)
     def server(self):
+        os.environ["strip-header"] = "1"
+        proc = Process(target=run_server, args=(), daemon=True)
+        proc.start()
+        yield
+        proc.terminate()
+        del os.environ["strip-header"]
+        while proc.is_alive():
+            sleep(1)
+
+
+@pytest.mark.parametrize("auth_mode", [AuthenticationScheme.AUTO, AuthenticationScheme.KERBEROS])
+class TestNegotiateMissingHeaderFailures(NegotiateTests):
+    @pytest.fixture(autouse=True)
+    def server(self):
+        # Remove all the routes (a bit drastic)
+        custom_test_app.router.routes = []
+
+        @custom_test_app.get("/")
+        async def get_forbidden(request: Request):
+            validate_user_principal(request, "otheruser@EXAMPLE.COM")
+            return None
+
         os.environ["strip-header"] = "1"
         proc = Process(target=run_server, args=(), daemon=True)
         proc.start()
