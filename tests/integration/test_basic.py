c4697fe9ec20fc6b27cfb1b955039ebae29f31eb
Andy Grigg <andrew.grigg@ansys.com>
2024-08-19 14:57:07 -0400

    Delete duplicate test cases

--- tests/integration/test_basic.py
+++ tests/integration/test_basic.py
@@ -89,24 +89,13 @@ def run_server():
     uvicorn.run(custom_test_app, port=TEST_PORT)
 
 
-class TestBasic:
-    @pytest.fixture(autouse=True)
-    def server(self):
-        proc = Process(target=run_server, args=(), daemon=True)
-        proc.start()
-        yield
-        proc.terminate()
-        while proc.is_alive():
-            sleep(1)
-
-    @pytest.mark.parametrize("auth_mode", [AuthenticationScheme.AUTO, AuthenticationScheme.BASIC])
+class BasicTestCases:
     def test_can_connect(self, auth_mode):
         client_factory = ApiClientFactory(TEST_URL, SessionConfiguration())
         _ = client_factory.with_credentials(
             TEST_USER, TEST_PASS, authentication_scheme=auth_mode
         ).connect()
 
-    @pytest.mark.parametrize("auth_mode", [AuthenticationScheme.AUTO, AuthenticationScheme.BASIC])
     def test_invalid_user_return_401(self, auth_mode):
         client_factory = ApiClientFactory(TEST_URL, SessionConfiguration())
         with pytest.raises(ApiConnectionException) as exception_info:
@@ -116,7 +105,6 @@ class TestBasic:
         assert exception_info.value.response.status_code == 401
         assert "Unauthorized" in exception_info.value.response.reason
 
-    @pytest.mark.parametrize("auth_mode", [AuthenticationScheme.AUTO, AuthenticationScheme.BASIC])
     def test_get_health_returns_200_ok(self, auth_mode):
         client_factory = ApiClientFactory(TEST_URL, SessionConfiguration())
         client = client_factory.with_credentials(
@@ -127,7 +115,6 @@ class TestBasic:
         assert resp.status_code == 200
         assert "OK" in resp.text
 
-    @pytest.mark.parametrize("auth_mode", [AuthenticationScheme.AUTO, AuthenticationScheme.BASIC])
     def test_patch_model(self, auth_mode):
         from .. import models
 
@@ -163,7 +150,20 @@ class TestBasic:
         assert response == deserialized_response
 
 
-class TestBasicWrongHeader:
+@pytest.mark.parametrize("auth_mode", [AuthenticationScheme.AUTO, AuthenticationScheme.BASIC])
+class TestBasic(BasicTestCases):
+    @pytest.fixture(autouse=True)
+    def server(self):
+        proc = Process(target=run_server, args=(), daemon=True)
+        proc.start()
+        yield
+        proc.terminate()
+        while proc.is_alive():
+            sleep(1)
+
+
+@pytest.mark.parametrize("auth_mode", [AuthenticationScheme.BASIC])
+class TestBasicWrongHeader(BasicTestCases):
     @pytest.fixture(autouse=True)
     def server(self):
         os.environ["change-header"] = "1"
@@ -175,67 +175,9 @@ class TestBasicWrongHeader:
         while proc.is_alive():
             sleep(1)
 
-    def test_can_connect(self):
-        client_factory = ApiClientFactory(TEST_URL, SessionConfiguration())
-        _ = client_factory.with_credentials(
-            TEST_USER, TEST_PASS, authentication_scheme=AuthenticationScheme.BASIC
-        ).connect()
 
-    def test_invalid_user_return_401(self):
-        client_factory = ApiClientFactory(TEST_URL, SessionConfiguration())
-        with pytest.raises(ApiConnectionException) as exception_info:
-            _ = client_factory.with_credentials(
-                "eve", "password", authentication_scheme=AuthenticationScheme.BASIC
-            ).connect()
-        assert exception_info.value.response.status_code == 401
-        assert "Unauthorized" in exception_info.value.response.reason
-
-    def test_get_health_returns_200_ok(self):
-        client_factory = ApiClientFactory(TEST_URL, SessionConfiguration())
-        client = client_factory.with_credentials(
-            TEST_USER, TEST_PASS, authentication_scheme=AuthenticationScheme.BASIC
-        ).connect()
-
-        resp = client.request("GET", TEST_URL + "/test_api")
-        assert resp.status_code == 200
-        assert "OK" in resp.text
-
-    def test_patch_model(self):
-        from .. import models
-
-        deserialized_response = models.ExampleModel(
-            string_property="new_model",
-            int_property=1,
-            list_property=["red", "yellow", "green"],
-            bool_property=False,
-        )
-
-        resource_path = "/models/{ID}"
-        http_method = "PATCH"
-        path_params = {"ID": TEST_MODEL_ID}
-
-        response_type = "ExampleModel"
-
-        upload_data = {"ListOfStrings": ["red", "yellow", "green"]}
-
-        client_factory = ApiClientFactory(TEST_URL, SessionConfiguration())
-        client = client_factory.with_credentials(
-            TEST_USER, TEST_PASS, authentication_scheme=AuthenticationScheme.BASIC
-        ).connect()
-        client.setup_client(models)
-
-        response = client.call_api(
-            resource_path,
-            http_method,
-            path_params=path_params,
-            body=upload_data,
-            response_type=response_type,
-            _return_http_data_only=True,
-        )
-        assert response == deserialized_response
-
-
-class TestBasicMissingHeader:
+@pytest.mark.parametrize("auth_mode", [AuthenticationScheme.BASIC])
+class TestBasicMissingHeader(BasicTestCases):
     @pytest.fixture(autouse=True)
     def server(self):
         os.environ["strip-header"] = "1"
@@ -246,62 +188,3 @@ class TestBasicMissingHeader:
         del os.environ["strip-header"]
         while proc.is_alive():
             sleep(1)
-
-    def test_can_connect(self):
-        client_factory = ApiClientFactory(TEST_URL, SessionConfiguration())
-        _ = client_factory.with_credentials(
-            TEST_USER, TEST_PASS, authentication_scheme=AuthenticationScheme.BASIC
-        ).connect()
-
-    def test_invalid_user_return_401(self):
-        client_factory = ApiClientFactory(TEST_URL, SessionConfiguration())
-        with pytest.raises(ApiConnectionException) as exception_info:
-            _ = client_factory.with_credentials(
-                "eve", "password", authentication_scheme=AuthenticationScheme.BASIC
-            ).connect()
-        assert exception_info.value.response.status_code == 401
-        assert "Unauthorized" in exception_info.value.response.reason
-
-    def test_get_health_returns_200_ok(self):
-        client_factory = ApiClientFactory(TEST_URL, SessionConfiguration())
-        client = client_factory.with_credentials(
-            TEST_USER, TEST_PASS, authentication_scheme=AuthenticationScheme.BASIC
-        ).connect()
-
-        resp = client.request("GET", TEST_URL + "/test_api")
-        assert resp.status_code == 200
-        assert "OK" in resp.text
-
-    def test_patch_model(self):
-        from .. import models
-
-        deserialized_response = models.ExampleModel(
-            string_property="new_model",
-            int_property=1,
-            list_property=["red", "yellow", "green"],
-            bool_property=False,
-        )
-
-        resource_path = "/models/{ID}"
-        http_method = "PATCH"
-        path_params = {"ID": TEST_MODEL_ID}
-
-        response_type = "ExampleModel"
-
-        upload_data = {"ListOfStrings": ["red", "yellow", "green"]}
-
-        client_factory = ApiClientFactory(TEST_URL, SessionConfiguration())
-        client = client_factory.with_credentials(
-            TEST_USER, TEST_PASS, authentication_scheme=AuthenticationScheme.BASIC
-        ).connect()
-        client.setup_client(models)
-
-        response = client.call_api(
-            resource_path,
-            http_method,
-            path_params=path_params,
-            body=upload_data,
-            response_type=response_type,
-            _return_http_data_only=True,
-        )
-        assert response == deserialized_response
